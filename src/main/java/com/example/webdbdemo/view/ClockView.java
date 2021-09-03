eaf1030106cdb7e9a89d211f6d4b46932252f33e
TothZoltan <toth2zoltan@gmail.com>
2021-09-03 23:02:24 +0200

    Clock demo start, stop

--- src/main/java/com/example/webdbdemo/view/ClockView.java
+++ src/main/java/com/example/webdbdemo/view/ClockView.java
@@ -2,9 +2,11 @@ package com.example.webdbdemo.view;
 
 import com.example.webdbdemo.model_spring.User;
 import com.example.webdbdemo.model_spring.UserRepository;
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Label;
@@ -20,24 +22,59 @@ import java.util.List;
 @PageTitle("Clock")
 @Route(value = "clock", layout = MainLayout.class)
 public class ClockView extends HorizontalLayout {
-
+    private Button pause;
 
     public ClockView() {
         add(new Html("""
             <div><p id="clock_element">Idő<p><div>
             """));
+        add(pause=new Button("Resume",this::pauseOnClick));
+    }
+
+    void pauseOnClick(ClickEvent event){
+        if(pause.getText().equals("Pause")){
+            pause.setText("Resume");
+            UI.getCurrent().getPage().executeJs("document.getElementById('clock_element').stopTimer();");
+        }else{
+            pause.setText("Pause");
+            UI.getCurrent().getPage().executeJs("document.getElementById('clock_element').startTimer();");
+        }
+
     }
 
     @PostConstruct
     public void init(){
 
         String string = """
+                
+                remainingTime=10*60*1000; // 10 perc
+                startTime=null;
+                
                 function showTime(){
-                    date=new Date().toLocaleTimeString("it-IT");
-                    document.getElementById('clock_element').innerHTML=date;
+                    if(startTime==null){
+                        actualTime=remainingTime;
+                    }else{
+                        actualTime=(startTime-new Date())+remainingTime
+                    }
+                    document.getElementById('clock_element').innerHTML=new Date(0,0,0,0,0,0,actualTime).toLocaleTimeString("it-IT");
                     setTimeout(showTime,1000);
                  }
-                 showTime();""";
+                 
+                 function startTimer(){
+                    startTime=new Date();
+                 }
+
+                 function stopTimer(){
+                    remainingTime=(startTime-new Date())+remainingTime;
+                    startTime=null;
+                 }
+                 
+                 clock=document.getElementById('clock_element')
+                 clock.startTimer=startTimer
+                 clock.stopTimer=stopTimer
+                 
+                 showTime();
+                 """;
         UI.getCurrent().getPage().executeJs(string);
     }
 }
